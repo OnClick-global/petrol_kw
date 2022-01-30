009b772c2a283f7ed79461d6dffef932b2e5f290
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-01-30 21:22:53 +0200

    SS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="des/css/style.css?V=1.2.2" rel="stylesheet">
+    <link href="des/css/style.css?V=1.2.3" rel="stylesheet">
     <title>{{ config('app.name', 'KOC') }}</title>
 </head>
 
