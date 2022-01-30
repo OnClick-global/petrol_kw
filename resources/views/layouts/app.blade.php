38cbfc3f72da4198021dd7c6c1e0f854f6831bfe
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-01-30 05:21:55 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="des/css/style.css?V=1.7" rel="stylesheet">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'KOC') }}</title>
 </head>
 
 @yield('content')
